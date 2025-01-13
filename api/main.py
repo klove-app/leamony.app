bf81f83e17a0df76337ab17d56a75c53e9a13fec
kloveapp <ivankazakov.newl@gmail.com>
2025-01-13 19:50:30 +0100

    fix: убран автозапуск веб-сервера из api/main.py

--- api/main.py
+++ api/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routers import auth, profile
-import uvicorn
 
 app = FastAPI(
     title="RunTracker API",
@@ -29,7 +28,4 @@ async def root():
         "status": "ok",
         "message": "RunTracker API работает",
         "version": "1.0.0"
-    }
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) 
+    } 
