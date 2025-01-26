9039eb5f1756947d29b2bcbe6ddccd05aac43253
kloveapp <ivankazakov.newl@gmail.com>
2025-01-26 21:24:06 +0100

    fix: use img tag for svg logo

--- nextjs/src/components/Navbar.tsx
+++ nextjs/src/components/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import { useAuth } from '@/lib/useAuth';
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function Navbar() {
   const { user, logout } = useAuth();
@@ -14,11 +13,9 @@ export default function Navbar() {
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
               <Link href="/" className="flex items-center space-x-2">
-                <Image 
+                <img 
                   src="/images/logo.svg" 
                   alt="RunConnect Logo" 
-                  width={32} 
-                  height={32}
                   className="w-8 h-8"
                 />
                 <span className="text-xl font-bold text-blue-600">RunConnect</span>
