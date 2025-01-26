c99861c2f525673c6017e680acb155a4e411c33a
kloveapp <ivankazakov.newl@gmail.com>
2025-01-26 17:43:51 +0100

    fix: remove App Router and update Pages Router structure

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -3,11 +3,22 @@ import { ErrorBoundary } from '@/components/ErrorBoundary'
 import Providers from '@/components/Providers'
 import '../styles/globals.css'
 
+// Импортируем шрифт Inter
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary>
       <Providers>
-        <Component {...pageProps} />
+        <div className={inter.variable}>
+          <Component {...pageProps} />
+        </div>
       </Providers>
     </ErrorBoundary>
   )
