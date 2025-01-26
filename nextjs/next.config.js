99fcda37248a2d8f52ca97af1a91a83fe63d4a8f
kloveapp <ivankazakov.newl@gmail.com>
2025-01-26 18:45:58 +0100

    feat: switch to static export

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -2,8 +2,8 @@
 const path = require('path')
 
 module.exports = {
-  experimental: {
-    outputStandalone: true
-  },
-  output: 'standalone'
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
 } 
